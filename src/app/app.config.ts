8a8fc193dc6de5a33bc2637a175ded79648437ef
AnissaBrihmat <121158683+AnissaBrihmat@users.noreply.github.com>
2026-06-05 11:41:20 +0200

    gestion 401 token expiré avec redirect login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './services/auth-interceptor';
 import { routes } from './app.routes';
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])), 
+    provideRouter(routes, withRouterConfig({ onSameUrlNavigation: 'reload' })), 
+    provideHttpClient(withInterceptors([authInterceptor])),
   ]
 };
--- src/app/services/auth-interceptor.ts
+++ src/app/services/auth-interceptor.ts
@@ -1,26 +1,57 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './auth';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-   // Récupère le service sans constructeur (syntaxe fonctionnelle)
-  const authService = inject(AuthService);
 
-  // Récupère le token depuis localStorage
+  // Récupère les services
+  const authService = inject(AuthService);
+  const router = inject(Router);
   const token = authService.getToken();
 
-  // Si token présent, on ajoute le header Authorization à chaque requête
+  // Si token présent, on clone la requête avec le header Authorization
   if (token) {
-
-    // Les requêtes sont immuables — on clone avec le header ajouté
     const clonedReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
 
-    // On envoie la requête clonée avec le token
-    return next(clonedReq);
+    // On envoie la requête clonée et on écoute les erreurs
+    return next(clonedReq).pipe(
+      catchError((error) => {
+
+        if (error.status === 401) {
+  console.log('401 détecté → logout + redirect');
+  authService.logout();
+  router.navigate(['/login']);
+}
+
+        // On relaie l'erreur pour que le composant puisse la gérer aussi
+        return throwError(() => error);
+      })
+    );
   }
 
-  // Pas de token — on envoie la requête sans modification
-  return next(req);
+  // Pas de token → requête normale sans modification
+  // Pas de token → requête normale mais on écoute quand même les erreurs
+return next(req).pipe(
+  catchError((error) => {
+    if (error.status === 401) {
+      console.log('401 détecté → logout + redirect');
+      authService.logout();
+      router.navigate(['/login']);
+    }
+    return throwError(() => error);
+  })
+);
+
+
+  // Token expiré
+  // → Angular envoie requête avec vieux token
+  //   → Symfony retourne 401
+  //     → catchError détecte le 401
+  //       → logout() supprime le token
+  //         → redirect vers /login
 };
